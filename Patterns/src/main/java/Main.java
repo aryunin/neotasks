ed3a43271de9edac51048eec2225014f2f4e3b45
aryunin <im.aryunin@gmail.com>
2023-07-27 22:28:42 +0300

    factory & singleton tested

--- Patterns/src/main/java/Main.java
+++ Patterns/src/main/java/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import Factory.Coffee;
+import Factory.CoffeeFactory;
+import Factory.CoffeeType;
+import Singleton.Logger;
 
+public class Main {
+    private static final Logger log = Logger.getInstance();
+
+    public static void main(String[] args) {
+        Coffee americano = CoffeeFactory.getCoffee(CoffeeType.AMERICANO);
+        log.classLogg(americano, "expected americano");
+        Coffee espresso = CoffeeFactory.getCoffee(CoffeeType.ESPRESSO);
+        log.classLogg(espresso, "expected espresso");
     }
 }
