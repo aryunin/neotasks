d1c2e19500a22d98132cfffba6af1dfa54d4890a
aryunin <im.aryunin@gmail.com>
2023-07-27 22:16:42 +0300

    Singleton

---
+++ Patterns/src/main/java/Singleton/Logger.java
@@ -0,0 +1,30 @@
+package Singleton;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+    private static final String prefix = "Log info:";
+    private static volatile Logger logger;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if(logger == null) {
+            synchronized (Logger.class){
+                if(logger == null) {
+                    logger = new Logger();
+                }
+            }
+        }
+        return logger;
+    }
+
+    public void classLogg(Object obj, String info) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yy");
+        System.out.println(
+                prefix + " " + LocalDate.now().format(dtf) + " - " + obj.getClass().getSimpleName() + " - " + info
+        );
+    }
+}
